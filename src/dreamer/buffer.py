2af245971f9153462678d44046a23b895795f5ba
Alex Inch <alexinch96@yahoo.com>
2026-09-24 12:25:44 +0100

    refine buffer implementation

--- src/dreamer/buffer.py
+++ src/dreamer/buffer.py
@@ -19,7 +19,7 @@ class BufferState:
     full: Bool[Array, ""]
     obs: Float[Array, "Capacity Time Height Width Channels"]
     acts: Float[Array, "Capacity Time Action"]
-    discounts: Float[Array, "Capacity Time"]
+    conts: Float[Array, "Capacity Time"]
     rewards: Float[Array, "Capacity Time"]
 
     @property
@@ -39,16 +39,19 @@ class BufferState:
         )
 
 
-def init_buffer(capacity: int = 1000) -> BufferState:
-    N, T, H, W, C = capacity, 1000, 64, 64, 3
-    action_size = 3
+def init_buffer(
+    capacity: int = 1000,
+    seq_len: int = 1001,
+    action_dim: int = 3,
+) -> BufferState:
+    N, T, H, W, C = capacity, seq_len, 64, 64, 3
 
     return BufferState(
         cursor=jnp.asarray(0, dtype=jnp.int32),
         full=jnp.asarray(False),
         obs=jnp.zeros((N, T, H, W, C), dtype=jnp.float32),
-        acts=jnp.zeros((N, T, action_size), dtype=jnp.float32),
-        discounts=jnp.zeros((N, T), dtype=jnp.float32),
+        acts=jnp.zeros((N, T, action_dim), dtype=jnp.float32),
+        conts=jnp.zeros((N, T), dtype=jnp.float32),
         rewards=jnp.zeros((N, T), dtype=jnp.float32),
     )
 
@@ -58,12 +61,11 @@ def add_episodes(
     state: BufferState,
     obs: Float[Array, "B T H W C"],
     acts: Float[Array, "B T A"],
-    discounts: Float[Array, "B T"],
+    conts: Float[Array, "B T"],
     rewards: Float[Array, "B T"],
 ) -> BufferState:
-    # These are static assertions, so fine during JIT tracing.
     chex.assert_type(obs, float)
-    chex.assert_equal_shape_prefix([obs, acts, discounts, rewards], prefix_len=2)
+    chex.assert_equal_shape_prefix([obs, acts, conts, rewards], prefix_len=2)
     chex.assert_rank(obs, 5)
 
     batch_size = obs.shape[0]
@@ -71,8 +73,8 @@ def add_episodes(
 
     end = state.cursor + batch_size
 
-    # We can't write this as one arange: if both the start and end are traced, JAX
-    # doesn't know the size - instead it's clearly fixed to `batch_size`.
+    # We can't write this as arange(start, end), because if both the start and end are traced, JAX
+    # doesn't know the size. This way, it's clearly fixed to `batch_size`.
     dt = state.cursor.dtype
     indices = (state.cursor + jnp.arange(batch_size, dtype=dt)) % capacity
 
@@ -81,7 +83,7 @@ def add_episodes(
         full=jnp.logical_or(state.full, end >= capacity),
         obs=state.obs.at[indices].set(obs),
         acts=state.acts.at[indices].set(acts),
-        discounts=state.discounts.at[indices].set(discounts),
+        conts=state.conts.at[indices].set(conts),
         rewards=state.rewards.at[indices].set(rewards),
     )
 
@@ -106,18 +108,20 @@ def get_batch(state, batch_size, seq_length, key):
         maxval=T - seq_length + 1,
     )
 
+    is_first = jnp.zeros((batch_size, seq_length), dtype=jnp.bool)
+    is_first = is_first.at[:, 0].set(True)
+
     # [B, L]
     time_indices = seq_starts[:, None] + jnp.arange(seq_length)[None, :]
-
-    # [B, 1], broadcasts against [B, L]
+    # [B, 1] - broadcasts against [B, L] when we used these to slice the arrays
     batch_indices = episode_indices[:, None]
 
     return DreamerBatch(
         image=state.obs[batch_indices, time_indices],
         prev_action=state.acts[batch_indices, time_indices],
-        discount=state.discounts[batch_indices, time_indices],
+        cont=state.conts[batch_indices, time_indices],
         reward=state.rewards[batch_indices, time_indices],
-        is_first=jnp.zeros((batch_size, seq_length), dtype=jnp.bool_),
+        is_first=is_first,
     )
 
 
@@ -130,36 +134,45 @@ def collect_new_rollouts(
 ) -> BufferState:
     # Technically this function overfills if num_eps doesn't neatly divide by num_envs.
     num_envs = envs.num_envs
+
     assert envs.spec is not None
     assert envs.spec.max_episode_steps is not None
     max_timesteps = envs.spec.max_episode_steps
     logger.info(f"Collecting {num_eps} rollouts with policy '{policy.__name__}'")
 
     for ep_ix in range(0, num_eps, num_envs):
-        rollout_obs = np.empty(shape=(num_envs, 1000, 64, 64, 3))
-        rollout_acts = np.empty(shape=(num_envs, 1000, 3))
-        rollout_discounts = np.empty(shape=(num_envs, 1000))
-        rollout_rewards = np.empty(shape=(num_envs, 1000))
+        rollout_obs = np.zeros(shape=(num_envs, 1001, 64, 64, 3))
+        rollout_acts = np.zeros(shape=(num_envs, 1001, 3))
+        rollout_conts = np.zeros(shape=(num_envs, 1001))
+        rollout_rewards = np.zeros(shape=(num_envs, 1001))
 
         obs, _ = envs.reset(seed=ep_ix * 250)
+        # The first entry for each episode is the initial state. Note we're implicitly setting the initial action and reward to zero, these
+        # correspond to the action and reward "before" the initial state, so that's fine.
+        rollout_obs[:, 0] = normalise_obs(crop_obs(obs))
+        rollout_conts[:, 0] = 1
 
-        for t in range(max_timesteps):
+        for t in range(1, max_timesteps + 1):
             subkey, key = jax.random.split(key)
             acts = policy(obs, subkey)
-            obs, rwds, _terminated, _truncated, _infos = envs.step(np.array(acts))
+            obs, rwds, terminated, truncated, _infos = envs.step(np.array(acts))
 
-            # TODO: process obs, rewards and acts properly
-            # TODO: also don't really have a proper continue predictor atm...
-            rollout_obs[:num_envs, t] = normalise_obs(crop_obs(obs))
-            rollout_acts[:num_envs, t] = acts
-            rollout_discounts[:num_envs, t] = 0.997
-            rollout_rewards[:num_envs, t] = rwds[0]
+            # We store the action a_t and the following observation o_t+1 in the same time step, since that's what
+            # we'll what to exist in a single batch later when we train the model
+            rollout_obs[:, t] = normalise_obs(crop_obs(obs))
+            rollout_acts[:, t] = acts
+            # store raw reward, transform happens in the loss calc
+            rollout_rewards[:, t] = rwds
+
+            # Add the episode end to train the continue predictor
+            episode_ended = np.logical_or(terminated, truncated)
+            rollout_conts[:, t] = np.where(episode_ended, 0, 1)
 
         buffer = add_episodes(
             buffer,
             rollout_obs,
             rollout_acts,
-            rollout_discounts,
+            rollout_conts,
             rollout_rewards,
         )
 
